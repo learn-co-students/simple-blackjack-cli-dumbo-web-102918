1b49305c6cd696fee84dd1431386120e3b7ddaf6
jessanderson222 <jessanderson222@gmail.com>
2018-10-08 22:43:50 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,33 +1,55 @@
 def welcome
+  puts "Welcome to the Blackjack Table"
   # code #welcome here
 end
 
 def deal_card
+  puts 1 + rand(11)
+  1 + rand(11)
   # code #deal_card here
 end
 
-def display_card_total
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
   # code #display_card_total here
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
+  "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
+  puts
+  answer = gets.chomp
   # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"# code #end_game here
 end
 
 def initial_round
+puts "Your cards add up to #{deal_card + deal_card}"
+deal_card + deal_card
   # code #initial_round here
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+puts "Type 'h' to hit or 's' to stay"
+prompt_user
+input = get_user_input
+
+if input == 's'
+  puts card_total
+  card_total
+elsif input == 'h'
+  puts card_total + deal_card
+  card_total + deal_card
+else
+  prompt_user
+  puts "Please enter a valid command"
+end
 end
 
 def invalid_command
@@ -41,4 +63,3 @@ end
 def runner
   # code runner here
 end
-    
