63f7d7429a8c8bf7c950cbc5a4114f0eef8dd651
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-11 22:31:25 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -4,7 +4,7 @@ require_relative "blackjack.rb"
 def runner
   welcome
   card_total = initial_round
-    until card_total >= 17
+    until card_total >= 21
       hit?(card_total)
       card_total
     end
